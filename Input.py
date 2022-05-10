eabb11576ae164cfe67aee1c5e728e98351935fb
José Cuji <104839643+JoseIJLK@users.noreply.github.com>
2022-05-10 07:25:08 -0500

    Add files via upload

---
+++ Input.py
@@ -0,0 +1,6 @@
+nombre=input("Ingrse su nombre: ")
+apellido=input("Ingrse su apellido: ")
+ubicacion=input("Ingrese su ubicacion: ")
+edad=input("Ingrse su edad: ")
+space=" "
+print("Buen dia",nombre+space+apellido,"de",edad,"años, usted se encuentra en",ubicacion)
