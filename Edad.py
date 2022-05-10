9f29b4fb019d32f491b97618b76fb2f3c9a1eb27
José Cuji <104839643+JoseIJLK@users.noreply.github.com>
2022-05-10 08:18:30 -0500

    Add files via upload

---
+++ Edad.py
@@ -0,0 +1,13 @@
+nombre=input("Ingrese su nombre: ")
+apellido=input("Ingrese su apellido: ")
+edad=input("Ingrese su edad: ")
+space=" "
+if edad>=0 and edad<=11:
+    print("Es un niño")
+elif edad>=12 and edad<=17:
+    print("Es un adolecente")
+elif edad>=18 and edad<=29:
+    print("Es un joven")
+
+
+
