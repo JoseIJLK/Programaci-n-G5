787032042a4567b4963cad4e962eef9d67a0402d
José Cuji <104839643+JoseIJLK@users.noreply.github.com>
2022-06-08 11:40:28 -0500

    Add files via upload

---
+++ tablas de multiplicar con while y condicionales.py
@@ -0,0 +1,34 @@
+num1=1
+while True:
+    n=int(input("Ingrese el numero de tablas: "))
+    if n<1:
+        print("Error el numero debe ser positivo")
+    else:
+        break
+while True:
+    m=int(input("Ingrese hasta que numero multiplicar: "))
+    if m<1:
+        print("Error el numero debe ser positivo")
+    else:
+        break
+while num1<=n:
+    print("La tabla de #",num1)
+    num2=1
+    suma=0
+    par=0
+    impar=0
+    while num2<=m:
+        r=num2*num1
+        print(num2,"x",num1,"=",r)
+        num2=num2+1
+        if r%2==0:  
+            par=par+1
+        else:
+            impar=impar+1
+        suma=suma+r
+        promedio=suma//15
+    num1=num1+1
+    print("La suma de los valores es:",suma)
+    print("El promedio es:",promedio)
+    print("Hay",par,"numeros pares")
+    print("Hay",impar,"numeros impares","\n")
