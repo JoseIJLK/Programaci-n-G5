539e9ff6913c459dbcf6833a20b0b23b3219b54e
José Cuji <104839643+JoseIJLK@users.noreply.github.com>
2022-06-10 07:33:08 -0500

    Add files via upload

---
+++ def multiplicacion.py
@@ -0,0 +1,7 @@
+def multiplicacion(a,b):
+    return a*b
+print(multiplicacion(3,4))
+
+def multiplicacion(a,b):
+    return
+print(multiplicacion(3,4))
---
+++ def resta3.py
@@ -0,0 +1,9 @@
+def resta(a,b):
+    print(a-b)
+    return a-b
+
+resultado=resta(10,5)
+print(resultado)
+
+suma=resultado-5
+print(suma)
